c107348eef1cd93c4aafb98fff2bbef888a0c0e2
John Doe <david.arfi69100@hotmail.fr>
2025-04-01 14:30:19 +0200

    React03

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,28 @@
-function PokemonCard () {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew", 
+    },
+  ];
+
+  function PokemonCard () {
+    const pokemon = pokemonList[1];
+
     return (
       <figure>
+        {pokemon.imgSrc ? (
         <img
         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
         alt="Bulbasaur"
       />
-      <figcaption>Bulbasaur</figcaption>
+      ) : (
+      <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
       </figure>
     );
   };
