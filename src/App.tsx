adcd5b4fbf800e1da5fc1963b6c0fe2269a90588
John Doe <david.arfi69100@hotmail.fr>
2025-04-01 17:11:24 +0200

    React06

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
@@ -14,11 +15,28 @@ const pokemonList = [
 ];
 
 function App() {
+  
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemon} />
+      <button type="button" onClick={() => setPokemonName( "mew" )}>
+        Afficher Mew
+      </button>
+      <button type="button" onClick={() => setPokemonName("bulbasaur")}>
+        Afficher Bulbasaur
+      </button>
     </div>
   );
 }
 
 export default App;
+
+
