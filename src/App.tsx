7496be16f6101c1013d57b517547c82ec165d72c
John Doe <david.arfi69100@hotmail.fr>
2025-04-02 17:29:59 +0200

    React7

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-
 import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
@@ -10,12 +9,26 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
-  
   const [pokemonName, setPokemonName] = useState("bulbasaur");
 
   const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
@@ -25,18 +38,17 @@ function App() {
   }
 
   return (
-    <div>
-      <PokemonCard pokemon={pokemon} />
-      <button type="button" onClick={() => setPokemonName( "mew" )}>
-        Afficher Mew
-      </button>
-      <button type="button" onClick={() => setPokemonName("bulbasaur")}>
-        Afficher Bulbasaur
-      </button>
-    </div>
+     <div> 
+       <PokemonCard pokemon={pokemon} />
+      <nav  >
+        {pokemonList.map((pokemon) => (
+            <button key={pokemon.name} type="button" onClick={() => setPokemonName(pokemon.name)}>
+             {pokemon.name}
+            </button>
+        ))}
+      </nav>
+     </div> 
   );
 }
 
 export default App;
-
-
